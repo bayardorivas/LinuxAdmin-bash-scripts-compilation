af192c2a30db337368d1ee56f34b60ffba9df61f
Bayardo Rivas <bayardo.rivas@gmail.com>
2025-09-20 16:09:20 -0600

    Change path to Nginx LetsEncrypt SSL certificates

--- backup/backup-s01.sh
+++ backup/backup-s01.sh
@@ -3,10 +3,17 @@
 # NOTE: Run this script as a user with appropriate permissions to access Nginx configuration files.
 #.      
 # Define variables
+# Color codes for terminal output
+RED='\e[31m'
+GREEN='\e[32m'
+BLUE='\e[34m'
+BOLD='\e[1m'
+RESET='\e[0m'
+
 NGINX_CONF="/etc/nginx/nginx.conf"
 NGINX_SITES="/etc/nginx/sites-available"
 NGINX_SITES_ENABLED="/etc/nginx/sites-enabled"
-NGINX_SSL="/etc/nginx/ssl"
+NGINX_SSL="/etc/letsencrypt"
 BACKUP_DIR="/home/"$USER"/backup/s01"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 ARCHIVE_NAME="nginx_backup_s01_$TIMESTAMP.tar.gz"   
@@ -16,12 +23,13 @@ mkdir -p "$BACKUP_DIR"
 cd "$BACKUP_DIR"  || { echo "Failed to change directory to "$BACKUP_DIR""; exit 1; }
 # Create the archive
 tar -czf "$ARCHIVE_NAME" "$NGINX_CONF" "$NGINX_SITES" "$NGINX_SITES_ENABLED" "$NGINX_SSL" || { echo "Failed to create archive "$ARCHIVE_NAME""; exit 1; }
-chown "$USER":"$USER" "$ARCHIVE_NAME"
 # Verify if the archive was created successfully
 if [ $? -ne 0 ]; then
-    echo "Error: Backup creation failed!"
+    echo -e "${RED}Error:${RESET} Backup creation failed!"
     exit 1
 fi
+chown "$USER":"$USER" "$ARCHIVE_NAME"
+
 echo "Backup created successfully: "$BACKUP_DIR"/"$ARCHIVE_NAME""
 # Remove backups older than 90 days
 find "$BACKUP_DIR" -type f -name "nginx_backup_s01_*.tar.gz" -mtime +90 -exec rm {} \; || echo "No old backups to remove."
