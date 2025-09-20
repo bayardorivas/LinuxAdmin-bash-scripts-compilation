cf14a08398e8e4ef77c7964c81b0f97499da6db6
Bayardo Rivas <bayardo.rivas@gmail.com>
2025-09-20 16:46:23 -0600

    Add path to DocumentRoot and Fail2ban configuration files to backup

--- backup/backup-s01.sh
+++ backup/backup-s01.sh
@@ -15,10 +15,12 @@ NGINX_CONF="/etc/nginx/nginx.conf"
 NGINX_SITES="/etc/nginx/sites-available"
 NGINX_SITES_ENABLED="/etc/nginx/sites-enabled"
 NGINX_SSL="/etc/letsencrypt"
+DOC_ROOT="/var/www/"
+FAIL2BAN="/etc/fail2ban"
 BACKUP_DIR="/home/"$ADMIN_USER"/backup/s01"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 ARCHIVE_NAME="nginx_backup_s01_$TIMESTAMP.tar.gz"   
-
+MYSQL_DUMP_NAME="mysql_backup_s01_$TIMESTAMP.sql.gz"
 # TODO: Add user environment variable definition for admin tasks
 # Ensure USER variable is set
 if [ -z "$ADMIN_USER" ]; then
@@ -28,8 +30,9 @@ fi
 # Create backup directory if it doesn't exist
 mkdir -p "$BACKUP_DIR"
 cd "$BACKUP_DIR"  || { echo "Failed to change directory to "$BACKUP_DIR""; exit 1; }
+
 # Create the archive
-tar -czf "$ARCHIVE_NAME" "$NGINX_CONF" "$NGINX_SITES" "$NGINX_SITES_ENABLED" "$NGINX_SSL" || { echo "Failed to create archive "$ARCHIVE_NAME""; exit 1; }
+tar -czf "$ARCHIVE_NAME" "$NGINX_CONF" "$NGINX_SITES" "$NGINX_SITES_ENABLED" "$NGINX_SSL" "$DOC_ROOT" "$FAIL2BAN" "$MYSQL_DUMP_NAME" || { echo "Failed to create archive "$ARCHIVE_NAME""; exit 1; }
 # Verify if the archive was created successfully
 if [ $? -ne 0 ]; then
     echo -e "${RED}Error:${RESET} Backup creation failed!"
